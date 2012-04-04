71072f9d4e867cc3e50a9f37ac9caa25a783265d
Josh Levey <jll09@hampshire.edu>
2012-04-04 11:16:47 -0400

    updated files

--- files/player.py
+++ files/player.py
@@ -1,5 +1,23 @@
+import os, sys
 import pygame
 from pygame.locals import *
 from pygame.sprite import Sprite, Group
 from pygame import Surface
 
+class Player(Sprite):
+    def __init__(self,loc,bounds):
+        Sprite.__init__(self)
+        self.image, self.rect = load_image('robotzim.bmp', -1)
+        self.rect.center = loc
+        
+    
+    def update(self):
+        self.rect.clamp_ip(self.bounds) #stays within bounds
+
+    def carry(self):
+        if not self.alive():
+            return
+
+   
+    
+        
